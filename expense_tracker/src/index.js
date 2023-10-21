ef726c5cb354343b64e7f2356bfd00f34debf1e4
ABHISHEK BADIGER <86523668+codeAbhi2000@users.noreply.github.com>
2023-10-21 08:25:00 +0530

    final commit

--- expense_tracker/src/index.js
+++ expense_tracker/src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/expense_tracker'>
+    <BrowserRouter>
     <UserState>
       <App />
     </UserState>
