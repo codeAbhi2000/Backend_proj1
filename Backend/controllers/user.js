8752365faeeb36aa8666133b92bde463434dd8a4
ABHISHEK BADIGER <86523668+codeAbhi2000@users.noreply.github.com>
2023-10-25 09:37:13 +0530

    modified the user controller

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -15,43 +15,7 @@ const AWS = require('aws-sdk')
 
 const myToken = "$moBhi$Love430"
 
-const uploadToS3 = async (data, fileName) => {
-    // console.log(data);
-    const blob = new Blob([data], { type: 'application/pdf' });
-    const buffer = await blob.arrayBuffer();
-    const bufferData = Buffer.from(buffer);
 
-    AWS.config.update({
-        accessKeyId: process.env.AWS_S3_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_S3_ACCESS_SECRET_KEY,
-
-    });
-
-    const s3 = new AWS.S3()
-    const params = {
-        Bucket: 'abhshekexpenseapp',
-        Key: fileName,
-        Body: bufferData,
-        ContentType: 'application/pdf',
-        ACL: 'public-read',
-    };
-
-    return new Promise(async (resolve, reject) => {
-        s3.upload(params, (err, data) => {
-            if (err)
-            {
-                console.error('S3 upload error', err);
-                reject(err)
-            } else
-            {
-                //   console.log('File uploaded to S3:', data.Location);
-                resolve(data.Location)
-                // You can access the S3 URL in the data.Location property
-            }
-        });
-
-    })
-}
 
 
 exports.postSignUpUser = async (req, res, next) => {
@@ -129,90 +93,7 @@ exports.postLoginUser = async (req, res, next) => {
     }
 }
 
-exports.getAllUserExpenses = (req, res) => {
-    const uid = req.params.id
-    try
-    {
 
-        Expense.getAllExpenses(uid)
-            .then((data) => {
-                res.json({
-                    data: data[0]
-                })
-            }).catch(err => {
-                console.log(err)
-            })
-    } catch (error)
-    {
-        res.status(500).json({
-            msg: 'Something went wrong'
-        })
-    }
-}
-
-exports.getCatExpenses = (req, res) => {
-    const uid = req.params.id
-    try
-    {
-
-        Expense.getCatExpenses(uid)
-            .then((data) => {
-                res.json({
-                    data: data[0]
-                })
-            }).catch(err => console.log(err))
-    } catch (error)
-    {
-        res.status(500).json({
-            msg: 'Something went wrong'
-        })
-    }
-}
-
-
-exports.getBudgetLimit = (req, res) => {
-    const uid = req.params.id
-    try
-    {
-
-        Limit.getBudgetLimit(uid)
-            .then((data) => {
-                res.json({
-                    data: data[0]
-                })
-            }).catch(err => console.log(err))
-    } catch (error)
-    {
-        res.status(500).json({
-            msg: 'Something went wrong'
-        })
-    }
-}
-
-exports.postAddExpense = (req, res) => {
-    const id = req.body.id
-    const desc = req.body.description;
-    const amount = req.body.amount;
-    const date = req.body.date;
-    const uid = req.body.uid
-    const cat_id = req.body.cat_id;
-    try
-    {
-
-        const expense = new Expense(id, desc, date, amount, cat_id, uid)
-        expense.save()
-            .then(() => {
-                res.json({
-                    msg: 'Expense added successfully'
-                })
-            }).catch(err => console.log(err))
-    } catch (error)
-    {
-        res.status(500).json({
-            msg: 'Something went wrong'
-        })
-    }
-}
 
 exports.getOverAllUserDeatails = (req, res) => {
     const id = req.params.id;
@@ -233,85 +114,10 @@ exports.getOverAllUserDeatails = (req, res) => {
     }
 }
 
-exports.postAddBudget = async (req, res) => {
-    const income = req.body.income;
-    const budget = req.body.budget;
-    const savings = req.body.savings;
-    const date = req.body.date;
-    const uid = req.body.uid;
-    const cat_id_limit = req.body.cat_id_limit
-    const ubudget = new Budget(income, budget, savings, date, uid)
-    const catLimits = new Limit(cat_id_limit, date, uid)
-    try
-    {
-        await ubudget.save()
-        await catLimits.save()
-
-        res.json({
-            msg: 'Budget added successfully'
-        })
-    } catch (error)
-    {
-        console.log(error);
-        res.json({
-            msg: 'something went wrong'
-        })
-    }
-}
 
 
-exports.postGetMonthReport = async (req, res, next) => {
-    const { month, uid } = req.body
-    try
-    {
-        const data = await Expense.getMonthReport(month, uid)
-        //  console.log(data);
-        res.json({
-            data: data
-        })
-    } catch (error)
-    {
-        res.json({
-            msg: 'something went wrong'
-        })
-    }
 
-}
 
-exports.postGetYearReport = async (req, res, next) => {
-    const { year, uid } = req.body
-    try
-    {
-        const data = await Expense.getYearReport(year, uid)
-        //  console.log(data);
-        res.json({
-            data: data
-        })
-    } catch (error)
-    {
-        res.json({
-            msg: 'something went wrong'
-        })
-    }
-
-}
-
-exports.postGetReportGivenRange = async (req, res, next) => {
-    const { start_date, end_date, uid } = req.body
-    try
-    {
-        const data = await Expense.getReportGivenRange(start_date, end_date, uid)
-        //  console.log(data);
-        res.json({
-            data: data
-        })
-    } catch (error)
-    {
-        res.json({
-            msg: 'something went wrong'
-        })
-    }
-}
 
 exports.purchasePremium = (req, res) => {
     const id = req.params.id
@@ -438,50 +244,3 @@ exports.resetPassword = async (req, res) => {
     }
 }
 
-exports.postDownloadReport = async (req, res) => {
-    const { uid } = req.body
-    // console.log('Request Body:', req.body);
-    // console.log('Request Headers:', req.headers);
-    const formData = req.files.pdfFile
-    // console.log(req.files);
-    // console.log(uid);
-    // const formData = req.files.pdfFile
-    //  console.log(formData.data);
-    const fileName = `expenseReport${uid}/${new Date().getTime()}.pdf`
-    try
-    {
-        const fileUrl = await uploadToS3(formData.data, fileName)
-        // console.log(fileUrl);
-        const downlod = new Downloads(uid, fileUrl)
-        await downlod.save()
-        res.status(200).json({ fileUrl, msg: "successfully downloaded report" })
-    } catch (error)
-    {
-        console.log(error);
-        res.status(500).json({
-            msg: 'Something Went wrong'
-        })
-    }
-}
-
-exports.getDownloadList = async (req, res) => {
-    const uid = req.params.uid
-    try
-    {
-
-        const list = await Downloads.getAllDownloads(uid)
-        if (list)
-        {
-            res.json({
-                data: list[0]
-            })
-        }
-        // console.log(list);
-    } catch (error)
-    {
-        console.log(error);
-        res.status(500).json({
-            msg: 'something went Wrong'
-        })
-    }
-}
